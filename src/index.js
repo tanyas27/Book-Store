8d77c54db1e068957ebefa71c796a67b4335a0d3
Tanya Singh <tanyacs27@gmail.com>
2020-07-05 21:02:19 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
